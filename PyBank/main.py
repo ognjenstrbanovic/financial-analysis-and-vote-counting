8cf2a3c1f977ff004230e09cc4e9c7ea69fdf235
ognjenstrbanovic <ogistrbanovic@gmail.com>
2020-02-18 16:12:26 -0500

    second commit

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,53 +1,56 @@
-# Importing the os module allows us to create file paths across operating systems
+# Importing the "os" module allows us to create file paths across different operating systems
 import os
-# The csv module is for reading CSV files
+# Importing the "csv" module allows us to read CSV (comma-separated values) files
 import csv
-# Set the path for the file
+# Directly below, we are setting the path for the file
 csv_path = os.path.join("PyBank", "budget_data.csv")
-# Open the CSV file (with the delimiter at the comma)
+# We are opening the file with the delimiter set to be the comma (we do this with a "with" statement so that we do not have to close the file)
 with open(csv_path, newline="") as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=",")
-    # For readability, print the following to the terminal...
-    print("Financial Analysis")
-    print("----------------------------")
-    # For loop through the CSV file to calculate the total number of months included in the dataset, the net total amount of profit or losses over the entire period, as well as the average of the changes in Profit/Losses over the entire period (using the next function to exclude the header)
+    # Next, as per the instructions (and for readability), we print the following line to the terminal ("\n" inserts a newline after its placement)
+    print("Financial Analysis\n----------------------------")
+    # Use "for" to loop through the file, and calculate: the total number of months included in the dataset, the net total amount of profit or losses over the entire period, as well as the average of the changes in Profit/Losses over the entire period (using the "next" function to exclude the header)
     csv_header = next(csv_reader)
-    # Create two empty lists and append items to them through iteration
-    total_number_of_months = []
-    net_total_amount_of_profit_or_losses = []
+    # Create two empty lists, and append items to them through row iteration
+    total_number_months = []
+    net_total_amount_profit_losses = []
     for row in csv_reader:
-        total_number_of_months.append(row[0])
-        net_total_amount_of_profit_or_losses.append(int(row[1]))
-    # Print to the terminal using f-strings
-    print(f"Total Months: {len(total_number_of_months)}")
-    print(f"Total: ${sum(net_total_amount_of_profit_or_losses)}")
-    index = 0
-    profit_or_loss = []
-    while index < (len(total_number_of_months) - 1):
-        profit_or_loss.append(int(net_total_amount_of_profit_or_losses[index + 1]) - int(net_total_amount_of_profit_or_losses[index]))
-        index += 1
-    average_of_changes_in_profit_or_losses = 0
-    for element in profit_or_loss:
-        average_of_changes_in_profit_or_losses += float(element) / len(profit_or_loss)
-    # Rounding to 2 decimal places...
-    rounded_average_of_changes_in_profit_or_losses = round(average_of_changes_in_profit_or_losses, 2)
-    print(f"Average Change: ${rounded_average_of_changes_in_profit_or_losses}")
-    greatest_increase_profits_amount = max(profit_or_loss)
-    profit_or_loss.index(1926159)
-    greatest_increase_profits_date = total_number_of_months[24 + 1]
-    # Formatted strings didn't seem to work at first, so I used the str() method
-    print("Greatest Increase in Profits: " + str(greatest_increase_profits_date) + " ($" + str(greatest_increase_profits_amount) + ")")
-    greatest_decrease_losses_amount = min(profit_or_loss)
-    profit_or_loss.index(-2196167)
-    greatest_decrease_losses_date = total_number_of_months[43 + 1]
+        total_number_months.append(row[0])
+        net_total_amount_profit_losses.append(int(row[1]))
+    # Print to the terminal (using f-strings) to check if our calculations are correct
+    print(f"Total Months: {len(total_number_months)}")
+    print(f"Total: ${sum(net_total_amount_profit_losses)}")
+    # Use a "while" loop with the index, "i", starting at 0 and ending at 85 (one minus the length of the list because indexing starts at 0)
+    i = 0
+    # The empty list will contain the values of the month-to-month changes in Profit/Losses
+    profit_losses = []
+    while i < (len(total_number_months) - 1):
+        profit_losses.append(int(net_total_amount_profit_losses[i + 1]) - int(net_total_amount_profit_losses[i]))
+        i += 1
+    # Now calculate the average of each element in the list "profit_losses"; initialize the new variable to 0
+    average_changes_profit_losses = 0
+    for element in profit_losses:
+        average_changes_profit_losses += float(element) / len(profit_losses)
+    # Rounding to two decimal places for presentation purposes, and then printing to the terminal
+    rounded_average_changes_profit_losses = round(average_changes_profit_losses, 2)
+    print(f"Average Change: ${rounded_average_changes_profit_losses}")
+    # Next up is to look for maximum profit change and minimum losses change, which we do by checking our output and using that in the built-in ".index" function
+    greatest_increase_profit_amount = max(profit_losses)
+    profit_losses.index(1926159)
+    # We look for their dates; we add 1 because this is the month in which the greatest +/- change occurred
+    greatest_increase_profit_date = total_number_months[24 + 1]
+    # Used str() method below b/c formatted strings didn't seem to work after a couple of tries
+    print("Greatest Increase in Profits: " + str(greatest_increase_profit_date) + " ($" + str(greatest_increase_profit_amount) + ")")
+    greatest_decrease_losses_amount = min(profit_losses)
+    profit_losses.index(-2196167)
+    greatest_decrease_losses_date = total_number_months[43 + 1]
     print("Greatest Decrease in Losses: " + str(greatest_decrease_losses_date) + " ($" + str(greatest_decrease_losses_amount) + ")")
-# Specify the file to write to and open a file using "write" mode in order to export a text file with the results
-output_path = os.path.join("PyBank_results.txt")
-with open(output_path, 'w', newline='') as txt_file:
-    # \n means newline
-    txt_file.write("Financial Analysis\n-------------------------\n")
-    txt_file.write(f"Total Months: {len(total_number_of_months)}""\n")
-    txt_file.write(f"Total: ${sum(net_total_amount_of_profit_or_losses)}""\n")
-    txt_file.write(f"Average Change: ${rounded_average_of_changes_in_profit_or_losses}""\n")
-    txt_file.write("Greatest Increase in Profits: " + str(greatest_increase_profits_date) + " ($" + str(greatest_increase_profits_amount) + ")""\n")
+# Specify the file to write to and open/export a text file with the results using "write" mode
+output_path = os.path.join("PyBank_results_1.txt")
+with open(output_path, "w", newline="") as txt_file:
+    txt_file.write("Financial Analysis\n----------------------------\n")
+    txt_file.write(f"Total Months: {len(total_number_months)}""/n")
+    txt_file.write(f"Total: ${sum(net_total_amount_profit_losses)}""\n")
+    txt_file.write(f"Average Change: ${rounded_average_changes_profit_losses}""\n")
+    txt_file.write("Greatest Increase in Profits: " + str(greatest_increase_profit_date) + " ($" + str(greatest_increase_profit_amount) + ")""\n")
     txt_file.write("Greatest Decrease in Losses: " + str(greatest_decrease_losses_date) + " ($" + str(greatest_decrease_losses_amount) + ")")
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,17 +1,17 @@
-# Importing the os module allows us to create file paths across operating systems
+# Importing the "os" module allows us to create file paths across different operating systems
 import os
-# The csv module is for reading CSV files
+# Importing the "csv" module allows us to read CSV (comma-separated values) files
 import csv
-# Set the path for the file
+# Directly below, we are setting the path for the file
 csv_path = os.path.join("PyPoll", "election_data.csv")
-# Open the CSV file (with the delimiter at the comma)
+# We are opening the file with the delimiter set to be the comma (we do this with a "with" statement so that we do not have to close the file)
 with open(csv_path, newline="") as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=",")
-# For readability, and per instructions, print the following to the terminal...
-    print("Election Results")
-    print("-------------------------")
-    # For loop through the CSV file to calculate the total number of votes cast, the complete list of candidates who received who received votes, the percentage of votes that each candidate won, as well as the total number of votes each candidate won (using the next function to exclude the header)
+    # Next, as per the instructions (and for readability), we print the following line to the terminal ("\n" inserts a newline after its placement)
+    print("Election Results\n-------------------------")
+    # Use "for" to loop through the file, and calculate: the total number of votes cast, the complete list of candidates who received votes, the percentage of votes that each candidate won, as well as the total number of votes that each candidate won (using the "next" function to exclude the header)
     csv_header = next(csv_reader)
+    # Create two empty lists, and append items to them through row iteration; initialize each candidate's counter to 0
     total_number_votes_cast = []
     candidate_list = []
     Khan_counter = 0
@@ -19,10 +19,12 @@ with open(csv_path, newline="") as csv_file:
     Li_counter = 0
     fourth_candidate_counter = 0
     for row in csv_reader:
+        # The first column of each row contains each of the votes cast
         total_number_votes_cast.append(row[0])
-        # We could potentially use a switch statement for all of these if statements
+        # The candidates appear in the third column; if they are not in my newly-created list, "candidate_list", then append them to said list
         if row[2] not in candidate_list:
             candidate_list.append(row[2])
+        # If, for example, "Khan" appears in column 3, then add a vote to his name via "Khan_counter"
         if row[2] == "Khan":
             Khan_counter += 1
         if row[2] == "Correy":
@@ -31,8 +33,10 @@ with open(csv_path, newline="") as csv_file:
             Li_counter += 1
         if row[2] == "O'Tooley":
             fourth_candidate_counter += 1
-    print(f"Total Votes: {len(total_number_votes_cast)}")
-    print("-------------------------")
+        # We could potentially use a switch statement instead of all of these "if" statements that are above
+    # Print to the terminal (using f-strings, which uses less syntax) to check if our calculations are correct
+    print(f"Total Votes: {len(total_number_votes_cast)}""/n""-------------------------")
+    # To get each candidate's percentage, we use the "format" function; then, we print for each
     Khan_unformatted = Khan_counter / len(total_number_votes_cast)
     Khan_percentage = format(Khan_unformatted, ".3%")
     print(f"{candidate_list[0]}: {Khan_percentage} ({Khan_counter})")
@@ -46,10 +50,11 @@ with open(csv_path, newline="") as csv_file:
     fourth_candidate_percentage = format(fourth_candidate_unformatted, ".3%")
     print(f"{candidate_list[3]}: {fourth_candidate_percentage} ({fourth_candidate_counter})")
     print("-------------------------")
+    # The results in terminal make it obvious to us who the winner is of the election
     print(f"Winner: {candidate_list[0]}")
     print("-------------------------")
-# Specify the file to write to and open a file using "write" mode in order to export a text file with the results
-output_path = os.path.join("PyPoll_results.txt")
+# Specify the file to write to and open/export a text file with the results using "write" mode
+output_path = os.path.join("PyPoll_results_1.txt")
 with open(output_path, 'w', newline='') as txt_file:
     txt_file.write("Election Results\n-------------------------\n")
     txt_file.write(f"Total Votes: {len(total_number_votes_cast)}""\n")
